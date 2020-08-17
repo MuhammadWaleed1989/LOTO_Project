8efb28b2995fa6c7c77ff3a0a3a91c5528f8b8a8
MuhammadWaleed1989 <61509214+MuhammadWaleed1989@users.noreply.github.com>
2020-08-18 01:47:24 +0500

    target achived - cell colored

--- clientApp/src/app/pages/games/games.component.ts
+++ clientApp/src/app/pages/games/games.component.ts
@@ -28,6 +28,12 @@ export class GamesComponent implements OnInit {
   // page
   currentpage: number;
   totalPages: number;
+  public tableArray = [];
+  public tableHeader = [];
+  HighlightRow: Number;
+  HighlightCol: Number;
+  highlightedColArray = [];
+  highlightedRowArray = [];
   typeValidationForm: FormGroup; // type validation form
   constructor(private gameService: GameService, private modalService: NgbModal, public formBuilder: FormBuilder) {
     this.typesubmit = false;
@@ -48,17 +54,17 @@ export class GamesComponent implements OnInit {
      */
     this.loadAllGames();
 
-
+    this.generateRondomNumber();
     /**
      * Type validation form
      */
     this.typeValidationForm = this.formBuilder.group({
       gameID: ['-1'],
       gameName: ['', [Validators.required]],
-      cell1Value: ['', [Validators.required]],
-      cell2Value: ['', [Validators.required]],
-      cell3Value: ['', [Validators.required]],
-      cell4Value: ['', [Validators.required]],
+      // cell1Value: ['', [Validators.required]],
+      // cell2Value: ['', [Validators.required]],
+      // cell3Value: ['', [Validators.required]],
+      // cell4Value: ['', [Validators.required]],
     });
   }
 
@@ -72,6 +78,53 @@ export class GamesComponent implements OnInit {
     });
   }
 
+  private generateRondomNumber() {
+
+    for (var i = 1; i <= 300; i++) {
+      var col = 'col' + i;
+      this.tableHeader.push(col);
+    }
+
+    var arr = [];
+    while (arr.length < 1800) {
+      var r = Math.floor(Math.random() * 1800) + 1
+      if (arr.indexOf(r) === -1) arr.push(r);
+    }
+
+    for (let i = 6; i > 0; i--) {
+      var sliced = arr.splice(0, Math.ceil(arr.length / i));
+      var jsonData = {};
+      for (var j = 0; j < sliced.length; j++) {
+        var k = j + 1;
+        jsonData['col' + k] = sliced[j];
+      }
+      this.tableArray.push(jsonData);
+    }
+
+  }
+  clickedEvent(row: any, col: any, eve: any) {
+    this.highlightedColArray.push(col);
+    this.highlightedRowArray.push(row);
+    this.HighlightCol = col;
+    this.HighlightRow = row;
+    eve.currentTarget.classList.add("active");
+    console.log(eve)
+    // const checked = eve.target.checked;
+    // if(checked){
+    //   var target = eve.currentTarget;
+
+    //   var pElement = target.parentElement.nextSibling;
+    //   pElement.innerText= "included";
+    // }
+    // else{
+    //   var target = eve.currentTarget;
+
+    //   var pElement = target.parentElement.nextSibling;
+    //   pElement.innerText= "excluded";
+    // }
+  }
+
+
   deleteGame(info: GameInfo) {
     this.gameService.deleteGame(info.gameID).subscribe((resp: Response) => {
       this.statusMessage = 'Record Deleted Successfully.',
@@ -84,26 +137,28 @@ export class GamesComponent implements OnInit {
     this.typesubmit = false;
     this.modalService.open(largeDataModal, {
       backdrop: 'static',
-      size: 'lg',
+      size: 'xl',
       scrollable: true
     });
     if (info) {
       this.typeValidationForm.patchValue({
         gameID: info.gameID,
-        gameName: info.gameName,
-        cell1Value: info.cell1Value,
-        cell2Value: info.cell2Value,
-        cell3Value: info.cell3Value,
-        cell4Value: info.cell4Value
+        gameName: info.gameName
+        // ,
+        // cell1Value: info.cell1Value,
+        // cell2Value: info.cell2Value,
+        // cell3Value: info.cell3Value,
+        // cell4Value: info.cell4Value
       });
     } else {
       this.typeValidationForm.patchValue({
         gameID: "-1",
-        gameName: "",
-        cell1Value: "",
-        cell2Value: "",
-        cell3Value: "",
-        cell4Value: "",
+        gameName: ""
+        // ,
+        // cell1Value: "",
+        // cell2Value: "",
+        // cell3Value: "",
+        // cell4Value: "",
       });
     }
 
@@ -127,10 +182,10 @@ export class GamesComponent implements OnInit {
       var object = {
         'gameID': Number(this.typeValidationForm.get('gameID').value),
         'gameName': this.typeValidationForm.get('gameName').value,
-        'cell1Value': Number(this.typeValidationForm.get('cell1Value').value),
-        'cell2Value': Number(this.typeValidationForm.get('cell2Value').value),
-        'cell3Value': Number(this.typeValidationForm.get('cell3Value').value),
-        'cell4Value': Number(this.typeValidationForm.get('cell4Value').value),
+        // 'cell1Value': Number(this.typeValidationForm.get('cell1Value').value),
+        // 'cell2Value': Number(this.typeValidationForm.get('cell2Value').value),
+        // 'cell3Value': Number(this.typeValidationForm.get('cell3Value').value),
+        // 'cell4Value': Number(this.typeValidationForm.get('cell4Value').value),
         'IsDeleted': false
       };
 
---
+++ clientApp/src/app/pages/gamestart/gamestart.model.ts
@@ -0,0 +1,8 @@
+export interface Users {
+    userID: number;
+    userName: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    isUserOnline: boolean;
+}
